66b561c937c4b189e9f76abdbef91a58138dba36
LioQing <s2010157@gmail.com>
2020-03-29 23:23:47 +0800

    Added more OOP

---
+++ programming/c#/OOP/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Hello
+{
+    class Car : Vehicle //Car class is a derived/child/sub class of Vehicle class
+    {
+        public string Name { get; private set; } //public getter and private setter
+
+        public string HonkSound { get; set; }//public getter and setter
+
+        public string Brand { get; set; }
+
+        private double _weight; //backup field
+        public double Weight //property
+        {
+            get
+            {
+                return _weight;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    _weight = value;
+                }
+            }
+        }
+
+        //Encapsulation
+        //Apart from the following 2 access modifier, there are also protected, internal
+        private bool CheckWeight(double aWeight) //private function for inside the class
+        {
+            return aWeight > 0;
+        }
+
+        public void Honk() //public for all scopes
+        {
+            Console.WriteLine("BEEP BEEP");
+        }
+
+        public Car(string aName, double aWeight, string aHonkSound = "BEEP", string aBrand = "Toyota", string aType = "Car") : base(aType) //Constructor
+        {
+            Name = aName;
+            Weight = aWeight;
+            HonkSound = aHonkSound;
+            this.Brand = aBrand; //this. assign value to the class itself(Polymorphism), can be left out
+        }
+    }
+}
---
+++ programming/c#/OOP/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Vehicle
+    {
+        string Type { get; set; }
+
+        public Vehicle(string aType = "Car")
+        {
+            Type = aType;
+        }
+    }
+}
