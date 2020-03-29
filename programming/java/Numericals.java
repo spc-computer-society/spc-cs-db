78092684ba9207e0af784c37336ebff87db44771
151044 <51814503+151044@users.noreply.github.com>
2020-03-29 17:24:53 +0800

    Number Calculations

---
+++ programming/java/Numericals.java
@@ -0,0 +1,13 @@
+package spc.compsoc.example;
+public class Numericals{
+  public static void main(String[] args){
+    System.out.println(420 + 69);
+    System.out.println(420 - 69);
+    System.out.println(420 * 69);
+    //Integer division (=6);
+    System.out.println(420 / 69);
+    //Decimal Division (=6.08695652174...);
+    System.out.println((double) 420 / 69);
+    System.out.println(420 % 69);
+    }
+  }
