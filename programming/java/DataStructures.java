53b5232ea40a7f91b1963dd50ed146d96ba88a7e
151044 <51814503+151044@users.noreply.github.com>
2020-03-29 18:50:39 +0800

    Data Structures

---
+++ programming/java/DataStructures.java
@@ -0,0 +1,43 @@
+package spc.compsoc.example;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DataStructures{
+  public static void man(String[] args){
+    List<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    for(int i : list){
+      System.out.println(i + "\n");//1 and 2
+    }
+    List<Integer> sub = list.subList(1,sub.size());//Sublist contains 2;
+    sub.forEach(System.out::println);
+    
+    Map<String,Integer> ids = new HashMap<>();
+    ids.put("Bash",30624700);
+    ids.put("Patchy",30624770);
+    ids.put("Zaggy",514202);
+    System.out.println(ids.get("Zaggy"));//Outputs 514202
+    System.out.println(ids.get("Zappies"));Outputs null
+    System.out.println(ids.getOrDefault("Zappies",0));//Outputs 0
+    System.out.println(ids.entrySet().stream().mapToInt(ent -> ent.getValue()).sum());//Returns sum of all ids
+    
+    Queue<Long> l = new ArrayBlockingQueue<>();
+    l.add(1l);
+    l.add(200l);
+    System.out.println(l.peek());//outputs 1, in queue = {1,200}
+    System.out.println(l.poll());//Outputs 1, in queue = {200}
+    
+    Set<String> set = new HashSet<>();
+    set.add("Hi");
+    set.add("Hi");
+    System.out.println(set.size());// 1
+    }
+  }
