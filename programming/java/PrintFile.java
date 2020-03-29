8b20baf36969f23f4cf35e40bb8acff696c55d9d
151044 <51814503+151044@users.noreply.github.com>
2020-03-29 17:11:23 +0800

    Path move change

--- programming/java/PrintFile.java
+++ programming/java/PrintFile.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 public class PrintFile{
   public static void main(String[] args){
     try{
-      Files.readAllLines(Paths.get("../common/PrintText.txt")).forEach(System.out::println);
+      Files.readAllLines(Paths.get("../_common/PrintText.txt")).forEach(System.out::println);
     }catch(IOException ioe){
       System.err.println("Unable to read file.");
       ioe.printStackTrace();
