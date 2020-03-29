81ee7705d56ca42c7803a4810b9bed5089fb95db
151044 <51814503+151044@users.noreply.github.com>
2020-03-29 17:18:56 +0800

    Data Types in Java

---
+++ programming/java/DataTypes.java
@@ -0,0 +1,21 @@
+package spc.compsoc.example;
+public class DataTypes{
+  public static void main(String[] args){
+    boolean b = true;
+    int i = 59;
+    long l =59l;
+    float f = 69.420f;
+    double d = 69.420;
+    char c = 'D';
+    String s = "I love ict";
+    System.out.println(b);
+    System.out.println(i);
+    System.out.println(l);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(c);
+    System.out.println(s);
+    }
+  }
+    
+    
