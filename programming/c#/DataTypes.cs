e524b442f68fdcc77a332d5b4848cc0a645dbc92
LioQing <s2010157@gmail.com>
2020-03-29 17:50:10 +0800

    added c# files

---
+++ programming/c#/DataTypes.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+            int i = 59;
+            long l = 59l;
+            float f = 69.420f;
+            double d = 69.420d;
+            char c = 'D';
+            string str = "I love ICT";
+
+            Console.WriteLine(b);
+            Console.WriteLine(i);
+            Console.WriteLine(l);
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(c);
+            Console.WriteLine(str);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ programming/c#/Numercials.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(420 + 69);
+            Console.WriteLine(420 - 69);
+            Console.WriteLine(420 * 69);
+            //Integer Division
+            Console.WriteLine(420 / 69);
+            //Decimal Division (2 ways)
+            Console.WriteLine((double)420 / 69);
+            Console.WriteLine(Decimal.Divide(420, 69)); //returns Decimal class (more precise)
+            Console.WriteLine(420 % 69);
+
+            Console.ReadLine();
+        }
+    }
+}
