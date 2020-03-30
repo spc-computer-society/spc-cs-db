1706322259997dac23cb2df89aeb4ee687f5a906
Hoyin7123 <hoyin.wong712@gmail.com>
2020-03-30 13:31:35 +0800

    Class.py added

---
+++ programming/dart/DataType.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+main() {
+  var i = 5;  //This is an integer.
+  var d = 13.31;  //This is a double.
+  var s = "This is a string.";  //This is a string.
+  var b = true;  //This is a boolean.
+  print(d.runtimeType);
+  print(i.runtimeType);
+  print(s.runtimeType);
+  print(b.runtimeType);
+}
