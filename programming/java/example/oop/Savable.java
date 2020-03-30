05df404a44d0a14387b91d090af4c160efbbb2ee
151044 <51814503+151044@users.noreply.github.com>
2020-03-30 11:43:25 +0800

    Dependency

---
+++ programming/java/example/oop/Savable.java
@@ -0,0 +1,33 @@
+/*
+ *     SBA-ICT
+ *     Copyright (C) 2020  Colin Chow
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.colin.games.uno.api;
+
+/**
+ * The interface representing that this object is savable.
+ * @author Colin
+ */
+@FunctionalInterface /*Not that it helps anyone*/
+public interface Savable {
+
+    /**
+     * Retrieves the save form of this object.
+     * @return The save string of this object
+     */
+    String saveForm();
+}
