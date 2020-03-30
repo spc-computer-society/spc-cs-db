f1492a82681ea5b410e96637f6630344378c7890
Hoyin7123 <hoyin.wong712@gmail.com>
2020-03-30 13:44:02 +0800

    Class.py added

---
+++ programming/dart/Numericals.dart
@@ -0,0 +1,8 @@
+main() {
+  print(420 + 69);
+  print(420 - 69);
+  print(420 * 69);
+  print(420 / 69);  //Division
+  print(420 ~/ 69);  //Integer Division
+  print(420 % 69); //Modular Division
+}
