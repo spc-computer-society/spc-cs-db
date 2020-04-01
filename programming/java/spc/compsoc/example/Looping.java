6c36aaf607ec2abcea81b468c9bc0246d5adcef6
151044 <51814503+151044@users.noreply.github.com>
2020-04-01 10:23:08 +0800

    The indentation... It bothers me
    Like a lot

--- programming/java/spc/compsoc/example/Looping.java
+++ programming/java/spc/compsoc/example/Looping.java
@@ -10,16 +10,16 @@ public class Looping{
     //Is possible to use for Arrays
     for(String s : ls){
       System.out.println(s);
-  }
-  //Second way
-  //Using forEach(), which exists in most collection classes
-  ls.forEach(s -> System.out.println(s));
-  //Note that forEach requires basic knowledge in lambda functions or method references
-  
-  /Third way
-  //Using .stream(), which exists in most collection classes, except Map which you need to convert into a set first.
-  ls.stream().forEach(System.out::println);
-  //Stream is not only limited to these methods, but also has other useful functions such as map and filter
-  //See the standard library for details.
-  //Note that this requires some knowledge of lambda functions
+    }
+    //Second way
+    //Using forEach(), which exists in most collection classes
+    ls.forEach(s -> System.out.println(s));
+    //Note that forEach requires basic knowledge in lambda functions or method references
+    
+    /Third way
+    //Using .stream(), which exists in most collection classes, except Map which you need to convert into a set first.
+    ls.stream().forEach(System.out::println);
+    //Stream is not only limited to these methods, but also has other useful functions such as map and filter
+    //See the standard library for details.
+    //Note that this requires some knowledge of lambda functions
 }
