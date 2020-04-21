0f8358c3f1e11248ee4ceb16f2a5deb2e72c0dc0
151044 <51814503+151044@users.noreply.github.com>
2020-04-21 10:32:18 +0800

    License and Packaging

--- programming/java/spc/compsoc/example/RandomUtil.java
+++ programming/java/spc/compsoc/example/RandomUtil.java
@@ -1,5 +1,5 @@
 /*
- *     SBA-ICT
+ *     Comp-Soc-DB
  *     Copyright (C) 2020  Colin Chow
  *
  *     This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.colin.utils;
+package spc.compsoc.example;
 
 import java.util.Random;
 
