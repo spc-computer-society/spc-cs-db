dac1d84a0fba0b3d25da9ecedc4c737bbac06526
Hoyin7123 <57891900+Hoyin7123@users.noreply.github.com>
2020-06-10 17:12:23 +0800

    Minor amendments

--- programming/python/ArgsAndKwargs.py
+++ programming/python/ArgsAndKwargs.py
@@ -7,18 +7,18 @@ If we do not know the amount or even the key of the arguments, we an use the abo
 # **kwargs(Keyword Arguments)
 def student(normal_arg, **info):  # Double * is needed for kwargs.
     print(normal_arg)
-    for key, value in info.items():  # We use for loop to look for the keys and values of the kwarg.
+    for key, value in info.items():  # Kwargs converts pairs of keywords and values into a dictionary.
         print(f"Key: {key}\n"
               f"Value: {value}")
 
 
 student("This is a normal arg.", name="Charles", gender="Male", age=15)
-# More than one kwargs can be passed.
+# More than one kwargs can be passed, keywords are required.
 
 
 # *args
 def mom(*fruits):  # Single * is needed for args
-    for item in fruits:
+    for item in fruits:  # Args transform multiple arguments into a tuple.
         print(f"My mom bought {item} in the market today.")
 
 
