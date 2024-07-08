b0ae90b02329c6eb259fd6c48e4ceebb2494681e
151044 <51814503+151044@users.noreply.github.com>
2024-07-08 10:12:17 +0800

    docs: Update Comments to elaborate on how to create comments

--- programming/java/spc/compsoc/example/Comments.java
+++ programming/java/spc/compsoc/example/Comments.java
@@ -1,14 +1,18 @@
 package spc.compsoc.example;
 
 public class Comments{
-//comments are ignored by the compiler
-//This is a single-line comment
+  // Comments are ignored by the compiler
+  // This is a single-line comment (starting with //)
 
-/*This is a 
-Multi-line
-Comment*/
+  // Start and end multi-line comments comments with a /* and a */ respectively
+  /* This is a 
+  Multi-line
+  Comment */
 
-/**
-*This is a javadoc comment. The javadoc tool creates documentation based on these.
-*/
+  /* These comments can be on a single line too! */
+
+  /**
+  * This is a javadoc comment. The javadoc tool creates documentation based on these.
+  * The main specification is here: https://docs.oracle.com/en/java/javase/21/docs/specs/man/javadoc.html
+  */
 }
